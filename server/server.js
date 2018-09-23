e0623307eb7b1e95d740a8a0a904e2c40d3b94d0
Vincent Desforges <desforges.vincent@gmail.com>
2018-09-23 20:28:18 +0100

    Add GET todo routes

--- server/server.js
+++ server/server.js
@@ -23,6 +23,14 @@ app.post('/todos', (req, res) => {
 });
 
 // GET
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, err => {
+    res.status(400).send(err);
+  });
+});
+
 // EDIT
 // DELETE
 
